6a5cc35a747459dc3f1e9ea8c8f41fb9a135e079
vsonthalia <123002395+vsonthalia@users.noreply.github.com>
2025-10-28 05:27:14 +0000

    parametrize pipeline

--- steps/04_orchestrate_jobs.sql
+++ steps/04_orchestrate_jobs.sql
@@ -1,5 +1,5 @@
 use role accountadmin;
-use schema quickstart_prod.gold;
+use schema quickstart_{{environment}}.gold;
 
 -- ========================================================================
 -- Declarative target table of pipeline
@@ -16,7 +16,7 @@ create or alter table vacation_spots (
     aquarium_cnt int,
     zoo_cnt int,
     korean_restaurant_cnt int
-) data_retention_time_in_days = 1;
+) data_retention_time_in_days = {{retention_time}};
 
 -- ========================================================================
 -- Task: Merge pipeline results into target table
